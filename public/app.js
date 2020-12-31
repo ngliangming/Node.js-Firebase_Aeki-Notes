fa95e3fa5a77d702059d6e14a8d39149cf35c451
Ng Liang Ming <ngliangming@gmail.com>
2020-12-31 17:25:24 +0800

    Update to page head/foot and function
    Made the "dragable area" to fit the page, even after moving content outside screen boundry.
    
    Made dynamic "screen boundry"
    
    fixed footer position to bottom
    
    fixed the "drag" function
    
    converted "drag" function to apps.js function

--- public/app.js
+++ public/app.js
@@ -3,6 +3,16 @@ document.addEventListener("DOMContentLoaded", event => {
     const app = firebase.app();
     console.log(app)
 
+    const db = firebase.firestore();
+
+    const user = db.collection('users').doc('ngliangming@gmail.com');
+
+    user.get()
+        .then(doc => {
+            const data = doc.data();
+            // document.write(data.name);
+        })
+
 });
 
 
@@ -19,4 +29,27 @@ function googleLogin() {
         })
         .catch(console.log)
 
+}
+
+
+
+//Drag and Drop function
+function drag_start(event) {
+    var style = window.getComputedStyle(event.target, null);
+    var str = (parseInt(style.getPropertyValue("left")) - event.clientX) + ',' + (parseInt(style.getPropertyValue("top")) - event.clientY) + ',' + event.target.id;
+    event.dataTransfer.setData("Text", str);
+}
+
+function drop(event) {
+    var offset = event.dataTransfer.getData("Text").split(',');
+    var dm = document.getElementById(offset[2]);
+    dm.style.left = (event.clientX + parseInt(offset[0], 10)) + 'px';
+    dm.style.top = (event.clientY + parseInt(offset[1], 10)) + 'px';
+    event.preventDefault();
+    return false;
+}
+
+function drag_over(event) {
+    event.preventDefault();
+    return false;
 }
