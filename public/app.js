50a5ead56e29bb24c3fdaa00ed29e3ca8d7db015
Ng Liang Ming <ngliangming@gmail.com>
2021-01-11 14:27:45 +0800

    Major changed
    -Added a button to add new notes
    -fixed css
    -added interactions with firestore db (create notes, update, delete)
    -fixed note top and content border on mobile (swapped css border to <hr> instead)

--- public/app.js
+++ public/app.js
@@ -17,15 +17,6 @@ document.addEventListener("DOMContentLoaded", event => {
     } else {
         console.log('not logged in');
     }
-
-    for (let i = 0; i < 10; i++) {
-        // add_note(i, "a", "", i * 50 + 50, i * 50 + 100);
-    }
-
-    // setTimeout(() => {
-    //     toggleSub();
-    //     console.log('unsub');
-    // }, 5000);
 });
 
 //Login functions
@@ -36,8 +27,6 @@ function googleLogin() {
 
         .then(result => {
             const user = result.user;
-            // document.write(`Hello ${user.displayName}`);
-            //console.log(user.email)
 
             localStorage.setItem('user', user.displayName);
             localStorage.setItem('email', user.email);
@@ -69,12 +58,6 @@ function loadUser() {
     user = localStorage.getItem('user');
 
     if (user) {
-        console.log(`Prior data exists as user ${user}`);
-        document.getElementById('options').style.display = "block";
-        document.getElementById('loginBtn').style.display = "none";
-
-        document.getElementById('welcomeMsg').innerHTML = `Welcome ${localStorage.getItem('user')}!`;
-
         window.user_cont = window.db.collection('users').doc(localStorage.getItem('email')).collection('notes');
 
         var oldNote = new Array();
@@ -112,7 +95,7 @@ function loadUser() {
                     const data = snap.docs[i].data();
 
                     //Check if note exists in browser
-                    if (document.getElementById(`card_${snap.docs[i].id}`) == null) {
+                    if (document.getElementById(`${snap.docs[i].id}`) == null) {
                         //Note does not exist in browser, add new note
                         add_note(snap.docs[i].id, data['title'], data['content'], data['x'], data['y']);
                         console.log(`new note id ${snap.docs[i].id} added`);
@@ -129,21 +112,42 @@ function loadUser() {
                 console.log("=============== Update complete ===============")
             });
 
-
-            document.getElementById('welcomeMsg').innerHTML = `Welcome ${localStorage.getItem('user')}!`;
+            toggleUi(true);
         });
 
         return true;
     }
     else {
-        console.log(`No prior data`);
-        document.getElementById('loginBtn').style.display = "block";
-        document.getElementById('options').style.display = "none";
+        toggleUi(false);
+
+        return false;
+    }
+
+}
+
+function toggleUi(loggedIn) {
+    options = document.getElementById('options');
+    loginBtn = document.getElementById('loginBtn');
+    welcomeMsg = document.getElementById('welcomeMsg');
+    createBtn = document.getElementById('createNote');
+
+    if (loggedIn) {
+        console.log(`Prior data exists as user ${user}`);
+        options.style.display = "block";
+        loginBtn.style.display = "none";
+        welcomeMsg.style.opacity = 0;
+        
+        createBtn.style.display = "block";
+        
+    } else {
+        // console.log(`No prior data`);
+        loginBtn.style.display = "block";
+        options.style.display = "none";
+        welcomeMsg.style.opacity = 1;
+
+        createBtn.style.display = "none";
 
         closeAll();
-
-        document.getElementById('welcomeMsg').innerHTML = "Welcome!";
-        return false;
     }
 
 }
@@ -158,20 +162,26 @@ function drag_start(event) {
 function drop(event) {
     var offset = event.dataTransfer.getData("Text").split(',');
     var dm = document.getElementById(offset[2]);
-
+    var x;
+    var y;
 
     if (event.clientX + parseInt(offset[1], 10) <= 50) {
         dm.style.left = 0 + 'px';
+        x = 0
     } else {
         dm.style.left = (event.clientX + parseInt(offset[0], 10)) + 'px';
-
+        x = event.clientX + parseInt(offset[0], 10);
     }
 
     if (event.clientY + parseInt(offset[1], 10) <= 50) {
         dm.style.top = 50 + 'px';
+        y = 50
     } else {
         dm.style.top = (event.clientY + parseInt(offset[1], 10)) + 'px';
+        y = event.clientY + parseInt(offset[1], 10);
     }
+
+    update_note(dm.id);
 
     event.preventDefault();
     return false;
@@ -190,7 +200,7 @@ function add_note(id, title, content, x, y) {
     //create div col
     var col = document.createElement("div");
     col.classList.add("col");
-    col.id = `card_${id}`;
+    col.id = `${id}`;
     col.draggable = true;
     col.addEventListener("dragstart", function () { drag_start(event) });
     col.style = `opacity: 0; left: ${x}px; top: ${y}px;`;
@@ -212,6 +222,7 @@ function add_note(id, title, content, x, y) {
     input.id = `title_${id}`;
     input.type = "text";
     input.value = title;
+    input.addEventListener("change", function () { update_note(id); });
 
     top_bar.appendChild(input);
 
@@ -219,16 +230,24 @@ function add_note(id, title, content, x, y) {
     var img = document.createElement("img");
     img.id = `close_${id}`;
     img.classList.add(`close_img_${id}`);
+    img.draggable = false;
     img.src = "https://icons.iconarchive.com/icons/custom-icon-design/mono-general-1/512/close-icon.png";
-    img.addEventListener('click', function () { setTimeout(close_note(this), 3000); });
+    // img.addEventListener('click', function () { setTimeout(close_note(this), 3000); });
+    img.addEventListener('click', function () { delete_note(id); });
 
     top_bar.appendChild(img);
+
+    //create HR
+    var hr = document.createElement("hr");
+
+    card.appendChild(hr);
 
     //create textarea > card
     var textarea = document.createElement("textarea");
     textarea.id = `content_${id}`;
     textarea.classList.add("content");
     textarea.innerHTML = content;
+    textarea.addEventListener("change", function () { update_note(id); });
 
     card.appendChild(textarea);
 
@@ -239,18 +258,7 @@ function add_note(id, title, content, x, y) {
     board.appendChild(col);
 
     //show new card
-    setTimeout(clear => document.getElementById(`card_${id}`).style.opacity = "1", 100);
-
-    // <div class="col" id="card_1" draggable="true" ondragstart="drag_start(event)" style="left: 862px; top: 630px;">
-    //     <div class="card">
-    //         <div class="top_bar">
-    //             <input id="title_1" type="text">
-    //             <img src="https://icons.iconarchive.com/icons/custom-icon-design/mono-general-1/512/close-icon.png" onclick="setTimeout(delete_note(this), 3000);">
-    //         </div>
-    //         <textarea id="content_1" class="content"></textarea>
-    //     </div>
-    // </div>
-
+    setTimeout(clear => document.getElementById(`${id}`).style.opacity = "1", 100);
 }
 
 function close_note(note) {
@@ -291,12 +299,79 @@ function closeAll() {
 
 function modify_note(id, title, content, x, y) {
     //Get elements
-    var cCol = document.getElementById(`card_${id}`);
+    var cCol = document.getElementById(`${id}`);
     var cTitle = document.getElementById(`title_${id}`);
     var cContent = document.getElementById(`content_${id}`);
 
-    //Modify value
-    cCol.style = `opacity: 1; left: ${x}px; top: ${y}px;`;
-    cTitle.value = title;
-    cContent.value = content;
+    //Check and modify differing values
+    if (cCol.style.left != x + "px" || cCol.style.top != y + "px") {
+        cCol.style = `opacity: 1; left: ${x}px; top: ${y}px;`;
+        // console.log(`${id} Change to x/y`)
+    }
+    if (cTitle.value != title) {
+        cTitle.value = title;
+        // console.log(`${id} Change to title`)
+    }
+    if (cContent.value != content) {
+        cContent.value = content;
+        // console.log(`${id} Change to content`)
+    }
+}
+
+function create_note() {
+    // window.user_cont = window.db.collection('users').doc(localStorage.getItem('email')).collection('notes')
+    window.db.collection('users').doc(localStorage.getItem('email')).collection('notes')
+        .add({
+            title: "",
+            content: "",
+            x: 100,
+            y: 150
+        })
+        .then(function () {
+            alert_box("Note created");
+            console.log("Note successfully created!");
+        })
+        .catch(function (error) {
+            console.error("Error creating note: ", error);
+        });
+}
+
+function delete_note(id) {
+    // window.db.collection('users').doc(localStorage.getItem('email')).collection('notes')
+    window.db.collection('users').doc(localStorage.getItem('email')).collection('notes').doc(id).delete()
+        .then(function () {
+            alert_box("Note deleted");
+            console.log("Note successfully deleted!");
+        })
+        .catch(function (error) {
+            console.error("Error deleting note: ", error);
+        });
+}
+
+function update_note(id) {
+    var cCol = document.getElementById(`${id}`);
+    var cTitle = document.getElementById(`title_${id}`);
+    var cContent = document.getElementById(`content_${id}`);
+
+    window.db.collection('users').doc(localStorage.getItem('email')).collection('notes').doc(id).set({
+        title: cTitle.value,
+        content: cContent.value,
+        x: cCol.style.left.replace("px", ""),
+        y: cCol.style.top.replace("px", "")
+    })
+        .then(function () {
+            alert_box("Note updated");
+            console.log("Note successfully updated!");
+        })
+        .catch(function (error) {
+            console.error("Error updated note: ", error);
+        });
+}
+
+function alert_box(msg) {
+    document.getElementById("alert").innerHTML = msg;
+    document.getElementById("alert").style.opacity = 1;
+    setTimeout(() => {
+        document.getElementById("alert").style.opacity = 0;
+    }, 1000);
 }
